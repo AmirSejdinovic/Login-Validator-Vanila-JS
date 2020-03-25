486f255f5d41d4e77aa61ae4e859de113a6f6ab1
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-03-25 10:20:04 +0100

    Refactoring code part2

--- script.js
+++ script.js
@@ -31,9 +31,13 @@ function showSuccess(input) {
 }
 
 //Function for checking if the email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 //Creating the checkRequired function
 function checkRequired(inputArr) {
@@ -51,9 +55,44 @@ function getFiledName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+//Creating function for check length which I use to check lengt in my username and password fields
+//I pase in the argumetns of fucntion three parametars and it is the input filed, min length and max length
+function checkLength(input, min, max) {
+  //Creatnig the if statement where I check if the input lengt is les than min
+  //And in the else if block i check if the input velue larger than max allowed
+
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFiledName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFiledName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+  //If the value of input1 is not equal with input2 value than showError
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwrods do not match");
+  }
+}
+
 //Adding the eventlistenr on submit action
 form.addEventListener("submit", function(e) {
   e.preventDefault();
   //Calling the custom function
   checkRequired([username, email, password, password2]);
+
+  //CheckLength
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
